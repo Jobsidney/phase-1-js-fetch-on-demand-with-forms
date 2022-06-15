6df6c7a32048affa96379228736875debd0f852c
Jobsidney <jobsidney67@gmail.com>
2022-06-15 10:25:33 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const form =document.querySelector('form');
+    form.addEventListener('submit',event => {
+        event.preventDefault();
+        const input=document.querySelector('input#searchByID');
+        fetch(' http://localhost:3000/movies')
+        .then(response =>response.json())
+        .then(data =>{
+            const foundData=data.find(item => item.id==input.value);
+            const title=document.querySelector('#movieDetails h4');
+            const summary=document.querySelector('#movieDetails p')
+            title.innerText=foundData.title;
+            summary.innerText=foundData.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);

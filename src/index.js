f8dc73cb3a984467f916f1aaa43b5f812fd0d39d
Jobsidney <jobsidney67@gmail.com>
2022-06-15 10:19:26 +0300

    commit tree c5dd13745de97a6480e99c2dfbb359d03e0467bd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const init = () => {
-  
+    const form =document.querySelector('form');
+    form.addEventListener('submit',event => {
+        event.preventDefault();
+        const input=document.querySelector('input#searchByID');
+        fetch(' http://localhost:3000/movies')
+        .then(response =>response.json())
+        .then(data =>{
+            const foundData=data.find(item => item.id==input.value);
+            const 
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
